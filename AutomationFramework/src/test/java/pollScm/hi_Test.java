0560baa8191495fa9dcdc36c6d48849a198e1ddc
aparn <aparn@Aparna>
2024-11-20 14:27:05 +0530

    first push

---
+++ AutomationFramework/src/test/java/pollScm/hi_Test.java
@@ -0,0 +1,5 @@
+package pollScm;
+
+public class hi_Test {
+
+}
---
+++ AutomationFramework/src/test/java/pollScm/pomScmTest.java
@@ -0,0 +1,12 @@
+package pollScm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class pomScmTest {
+	@Test
+	public void pollScm() {
+		Reporter.log("pollScm class executed success", true);
+	}
+
+}
